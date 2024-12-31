760ead8890c4d9a8659c00816b4abd7155424dbf
Guilherme de Oliveira <coelhoposa@proton.me>
2024-12-31 10:54:50 -0300

    Alterado o leiaute do Script
    Todo o Script foi separado em funções, que poderão ajudar a expandir ele futuramente.

--- minimal-gnome.sh
+++ minimal-gnome.sh
@@ -1,18 +1,57 @@
-#! /bin/bash
-## Instalando o básico do Gnome: GDM, Gnome-Shell, Gnome Console e o Gnome Control Center
-sudo pacman -S --noconfirm gdm gnome-shell gnome-console gnome-control-center
-sudo systemctl enable gdm
+#!/usr/bin/env bash
+set -e
 
-## Instalando o Seletor de Perfil de Energia e a Impressora/Scanner
-sudo pacman -S --noconfirm power-profiles-daemon system-config-printer cups sane
-sudo systemctl enable cups
-sudo systemctl enable bluetooth
+instalarGnomeMini () {
+	## Instalando o básico do Gnome: GDM, Gnome-Shell, Gnome Console e o Gnome Control Center
+	echo -e "\nInstalando o GDM, Gnome Shell, Console e o Gnome Control Center e os demais pacotes básicos do Gnome e do Wayland\n"
+	sleep 2
+	pacman -S --noconfirm gdm gnome-shell gnome-console gnome-control-center
 
-## Instalando o Flatpak e alguns aplicativos dessa forma
-sudo pacman -S --noconfirm flatpak
-sudo flatpak install -y --noninteractive org.mozilla.firefox org.bluesabre.MenuLibre org.gnome.Weather org.gnome.clocks org.gnome.Calendar org.gnome.Loupe org.gnome.Music org.gnome.Totem org.gnome.Calculator org.gnome.TextEditor
+	echo -e "\nHabilitando o GDM\n"
+	systemctl enable gdm
+}
 
+instalarLaptopImpressoraScanner () {
+	## Instalando o Seletor de Perfil de Energia e a Impressora/Scanner
+	echo -e "\nInstalando o Seletor de Perfil de Energia, a Impressora e o SANE\n"
+	sleep 2
+	pacman -S --noconfirm power-profiles-daemon system-config-printer cups sane
+	
+	echo -e "\nHabilitando o CUPS\n"
+	systemctl enable cups
+
+	echo -e "\nHabilitando o Bluetooth\n"
+	systemctl enable bluetooth
+}
+
+instalarFlatpak () {
+	echo -e "\nInstalando o Flatpak\n"
+	pacman -S --noconfirm flatpak
+	
+	echo -e "\nInstalando alguns aplicativos \n	- Firefox \n	- MenuLibre \n	- Visualizador de Imagens Loupe\n	- Gnome Music\n	- Totem\n	- Calculadora\n	- Editor de Texto"
+	sleep 2
+	flatpak install -y --noninteractive org.mozilla.firefox org.bluesabre.MenuLibre org.gnome.Loupe org.gnome.Music org.gnome.Totem org.gnome.Calculator org.gnome.TextEditor
+}
+
+instalarExtras () {
 ## Instalando alguns extras
-sudo pacman -S --noconfirm webp-pixbuf-loader gnome-software sushi gnome-system-monitor gnome-disks baobab
+	echo -e "\nInstalando a visualização de arquivos em .webp, a Gnome Software, o Monitor do Sistema, o Gerenciador de Discos, o Analisador de Espaço em Disco e o Sushi\n"
+	sleep 2
+	pacman -S --noconfirm webp-pixbuf-loader gnome-software sushi gnome-system-monitor gnome-disk-utility baobab
+}
 
+flatpakExtraGnome () {
+	echo -e "\nInstalando alguns Extras:\n	- Meteorologia\n	- Relógio\n	- Calendário"
+	sleep 2
+	flatpak install -y --noninteractive org.gnome.Weather org.gnome.clocks org.gnome.Calendar
+	
+	echo -e "\nInstalando os Wallpapers do Gnome\n"
+	sleep 2
+	pacman -S --noconfirm gnome-backgrounds
+}
 
+instalarGnomeMini
+instalarFlatpak
+flatpakExtraGnome
+instalarLaptopImpressoraScanner
+instalarExtras
